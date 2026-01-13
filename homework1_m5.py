f5a57685da5ae4681fe8bfca9ab7be77f46afe2d
Евгений Лысенко <udjiedwards@gmail.com>
2026-01-14 01:47:19 +0200

    while for input

--- homework1_m5.py
+++ homework1_m5.py
@@ -1,17 +1,29 @@
-# fibonachi
+# fibonachi 
 def caching_fibonacci(): # The function returns a Fibonacci function with caching
     cache = {}
 
     def fibonacci(n): # The function returns the n-th Fibonacci number
         if n in cache: # Check if the result is already cached
             return cache[n]
-        if n <= 1: 
-            return n
+        if n <= 0: 
+            return 0
+        if n == 1:
+            return 1
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2) # Calculate Fibonacci number recursively
         return cache[n]
     
     return fibonacci
+
 fibonacci = caching_fibonacci() # Get the Fibonacci function with caching
-fib_input = int(input("Введіть число для обчислення числа Фібоначчі: ")) # Input from user
-print(f"Число Фібоначчі для {fib_input} = {fibonacci(fib_input)}") # Print the Fibonacci number for the input
-print(fibonacci(fib_input + 2))
+
+while True:
+    fib_input = input("Введіть число для обчислення числа Фібоначчі або Exit для виходу: ") # Input from user
+    try:
+       if fib_input.lower() == 'exit': # Exit condition
+          break
+       fib_input = int(fib_input) # Convert input to integer
+       print(f"Число Фібоначчі для {fib_input} = {fibonacci(fib_input)}") # Print the Fibonacci number for the input
+    except ValueError:
+       print("Будь ласка, введіть коректне число або 'Exit' для виходу.") # Error message for invalid input
+       continue
+    
