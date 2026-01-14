19e18e67d8ef644a26d4803c47097211038450db
Евгений Лысенко <udjiedwards@gmail.com>
2026-01-14 02:07:51 +0200

    add main

--- homework1_m5.py
+++ homework1_m5.py
@@ -14,16 +14,19 @@ def caching_fibonacci(): # The function returns a Fibonacci function with cachin
     
     return fibonacci
 
-fibonacci = caching_fibonacci() # Get the Fibonacci function with caching
+def main():
+    fibonacci = caching_fibonacci() # Get the Fibonacci function with caching
 
-while True:
-    fib_input = input("Введіть число для обчислення числа Фібоначчі або Exit для виходу: ") # Input from user
-    try:
-       if fib_input.lower() == 'exit': # Exit condition
-          break
-       fib_input = int(fib_input) # Convert input to integer
-       print(f"Число Фібоначчі для {fib_input} = {fibonacci(fib_input)}") # Print the Fibonacci number for the input
-    except ValueError:
-       print("Будь ласка, введіть коректне число або 'Exit' для виходу.") # Error message for invalid input
-       continue
-    
+    while True:
+        fib_input = input("Введіть число для обчислення числа Фібоначчі або Exit для виходу: ") # Input from user
+        try:
+           if fib_input.lower() == 'exit': # Exit condition
+               break
+           fib_input = int(fib_input) # Convert input to integer
+           print(f"Число Фібоначчі для {fib_input} = {fibonacci(fib_input)}") # Print the Fibonacci number for the input
+        except ValueError:
+            print("Будь ласка, введіть коректне число або 'Exit' для виходу.") # Error message for invalid input
+            continue
+
+if __name__ == "__main__":
+    main()
